1813241518dd59e667649eaefb352cf4d6729c64
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2022-11-16 11:07:04 +0530

    Time: 13 ms (66.48%), Space: 42.9 MB (79.44%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,31 +1,14 @@
 class Solution {
     public String reverseWords(String S) {
-        String s=S.trim();
-        String str1="";
-	    int k=0;
-        for(int i=k;i<s.length();i=k){
-             String str="";
-            for(int j=i;j<s.length();j++){
-                k=j;
-                char l=s.charAt(j);
-                if((int)l==32&&j!=0){
-                     char p=s.charAt(j-1);
-                     if((int)p!=32)
-                         str +=" ";
-                     break;
-                }
-                else
-                    str +=l;
-            }
-            if(k==s.length()-1){
-                str1=str+" "+str1;
-               k++;
-            }
-            else{
-                str1=str+str1;
-                k++;
-            }
+       return  reversedList(S);
+    }
+    public static String reversedList(String str){
+        str=str.trim();
+        String ans="";
+        String arr[]=str.split("\s+");
+        for(int i=arr.length-1;i>=0;i--){
+           ans=ans+arr[i]+" ";
         }
-         return str1.trim();
+        return ans.trim();
     }
 }
