5cc7d5fdaf3eec60e95dd62e1a6fd8190b511597
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-03-16 10:48:06 +0530

    Time: 8 ms (93.88%), Space: 51.9 MB (29.73%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,22 +10,29 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
+        
         ListNode currentNode=head;
+        
         int count=0;
+        // find the total number node in linklist
         while(currentNode!=null){
             count++;
             currentNode=currentNode.next;
         }
+        // create the array with length count
         int arr[]=new int[count];
         currentNode=head;
         int i=0;
+        // add the element in the array
         while(currentNode!=null){
             arr[i]=currentNode.val;
             i++;
             currentNode=currentNode.next;
         }
         Arrays.sort(arr);
+        // Again change the val of each node in the linklist according the sorting
         currentNode=head;
+        
         for(int j=0;j<arr.length;j++){
             currentNode.val=arr[j];
             currentNode=currentNode.next;
