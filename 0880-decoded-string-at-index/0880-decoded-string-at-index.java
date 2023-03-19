9bddf89cb6243e00961b37d6dc49f9a268274782
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-03-20 00:19:45 +0530

    Time: 1 ms (29.82%), Space: 40.8 MB (14.03%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String decodeAtIndex(String s, int k) {
+        long size=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch)){
+                size =size*Integer.parseInt(ch+"");
+            }
+            else{
+                size++;
+            }
+        }
+        
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+            k %=size;
+            if(k==0 && Character.isDigit(ch)==false){
+                return ch+"";
+            }
+            if(Character.isDigit(ch)){
+                size =size/Integer.parseInt(ch+"");
+            }
+            else{
+                size--;
+            }
+            
+        }
+        
+        return "";
+    }
+}
