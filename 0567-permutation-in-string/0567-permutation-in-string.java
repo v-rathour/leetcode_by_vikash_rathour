28908fd9e2def3d28ed4ff47b15145b8953e1f9e
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-03-24 13:39:58 +0530

    Time: 6 ms (57.91%), Space: 42.8 MB (15.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -4,29 +4,30 @@ class Solution {
     }
      public static boolean permutation(String s1,String s2){
         int arr[]=new int[26];
+        // Store the frequency of the character of s1 String in the arr array 
         for (int i = 0; i < s1.length(); i++) {
             char ch=s1.charAt(i);
             arr[ch-'a']++;
         }
        
-        
+        // Store the frequency of the character of s2 String in the arr array 
         int trap[]=new int[26];
         int ei=0;
         int si=0;
         while(ei<s2.length()){
-
+             // grow the sliding window
             char ch=s2.charAt(ei);
             trap[ch-'a']++;
             ei++;
             
-
+            // srink the sliding window
             while(arr[ch-'a']<trap[ch-'a'] && si<=ei){
                 
                  trap[s2.charAt(si)-'a']--;
                  si++;
 
             }
-           
+            // calculate the answer
             if(s1.length()==(ei-si)){
                 return true;
             }
