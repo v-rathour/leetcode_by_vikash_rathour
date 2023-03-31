61f6a269c68b4662fb3b74e0da52c8df7ffe77d9
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-03-31 21:30:25 +0530

    Time: 2 ms (31.68%), Space: 44.8 MB (90.54%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,37 +1,19 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,List<Integer>> map=new HashMap<>();
-        int arr[]=new int[2];
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])==false){
-                List<Integer> list=new ArrayList<>();
-                list.add(i);
-                map.put(nums[i],list);
-            }
-            else{
-               List<Integer> list=map.get(nums[i]);
-                list.add(i);
-                map.put(nums[i],list);
-            }
-        }
-        
-        for(int i=0;i<nums.length;i++){
-            int a=nums[i];
-            List<Integer> list=map.get(a);
-            list.remove(0);
-            map.put(a,list);
-            
-            int b=target-a;
-            
-            if(map.containsKey(b)){
-                arr[0]=i+1;
-                arr[1]=map.get(b).get(0)+1;
+    public int[] twoSum(int[] numbers, int target) {
+        int a[]=new int[2];
+        int i=0,j=numbers.length-1;
+    while(i<numbers.length-1){
+            if(numbers[i]+numbers[j]==target){
+                a[0]=i+1;
+                a[1]=j+1;
                 break;
             }
-            
-            
-        }
-        
-        return arr;
+            else if(numbers[i]+numbers[j]>target){
+               j--;
+            }
+            else{
+                i++;
+            }        }
+        return a;       
     }
 }
