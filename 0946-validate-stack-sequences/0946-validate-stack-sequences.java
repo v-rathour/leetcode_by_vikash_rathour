f2d82dfed5ac37bfa662a2080a6d4a6e4cc2250b
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-04-13 11:23:22 +0530

    Time: 2 ms (91.99%), Space: 42 MB (82.26%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        
+        // stack create to store the element of the of pushed array
         Stack<Integer> st=new Stack<>();
-       
+        // it is initializa for iterate the popped array
         int j=0;
         
-        
-        Stack<Integer> pushs=new Stack<>();
         for(int i=0;i<pushed.length;i++){
+            
             if(popped[j]!=pushed[i]){
                 st.push(pushed[i]);
             }
