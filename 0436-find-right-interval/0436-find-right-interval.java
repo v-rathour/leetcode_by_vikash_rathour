1f11b883b7f34aab481b2f045cc0d8bfeb6423c5
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-05-23 17:07:23 +0530

    Time: 12 ms (92.99%), Space: 46.1 MB (98.70%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -2,23 +2,27 @@ class Solution {
     public int[] findRightInterval(int[][] intervals) {
         int n=intervals.length;
         int answer[]=new int[n];
-        
+        // Create the HashMap to store the start time for each intervals
         HashMap<Integer,Integer> map=new HashMap<>();
+        // it is store the start time for each intervals
         int arr[]=new int[n];
         for(int i=0;i<n;i++){
             int strat=intervals[i][0];
             arr[i]=strat;
             map.put(strat,i);
         }
+        // sort the array
         Arrays.sort(arr);
         
         for(int i=0;i<n;i++){
-            int end=intervals[i][1];
             
+            int end=intervals[i][1];
+            // if rigth intervals is not exist
             if(arr[n-1]<end){
                 answer[i]=-1;
             }
             else{
+                // if rigth intervals exist
                 int first=0;
                 int last=n-1;
                 int ans=0;
