997e95dc4e30a8323f415983bc8b25d2cfc2d961
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-10-20 23:15:02 +0530

    Time: 11 ms (23.10%), Space: 44.1 MB (57.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -2,23 +2,39 @@ class Solution {
     
     public int change(int amount, int[] coins) {
         
-        int dp[] = new int[amount+1];
-        dp[0] = 1;
+       int [][]dp = new int[amount+1][coins.length+1];
         
-        
-        for(int j = 0;j<coins.length;j++){
-
-            for(int i = 1;i<dp.length;i++){
-                
-                if(i >= coins[j] ){
-                    
-                    if(dp[i-coins[j]] != 0){
-                        dp[i] = dp[i]+dp[i-coins[j]];
-                    }
-                }
+        for (int i = 0; i < amount+1; i++) {
+            for (int j = 0; j < coins.length+1; j++) {
+                dp[i][j] = -1;
             }
         }
-        
-        return dp[amount];
+        return coinchange(coins,0,amount,dp);
+    }
+    
+    public static int coinchange(int []arr,int i,int amount,int [][]dp){
+
+        if(amount == 0){
+            return 1;
+        }
+
+        if(i == arr.length){
+            return 0;
+        }
+
+        if(dp[amount][i] != -1){
+            return dp[amount][i];
+        }
+
+        int inc = 0;
+        int exc = 0;
+
+        if(amount>=arr[i]){
+            inc = coinchange(arr,i,amount-arr[i],dp);
+        }
+        exc = coinchange(arr,i+1,amount,dp);
+
+        return dp[amount][i] = inc+exc;
+
     }
 }
