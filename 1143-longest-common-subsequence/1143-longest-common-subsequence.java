e310aafe5c35e0b480e431bd4624b2b4564f8b53
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-10-21 23:32:49 +0530

    Time: 33 ms (8.23%), Space: 48.2 MB (52.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -9,15 +9,16 @@ class Solution {
         return LCS(s1,s2,0,0,dp);
     }
     
-    public static int LCS(String s1,String s2,int i ,int j,int [][]dp){
+    public static int LCS(String s1,String s2,int i, int j,int [][]dp){
 
-        if(i == s1.length() || j == s2.length()){
-            return 0 ;
+        if(s1.length() == i || s2.length() == j){
+            return 0;
         }
 
         if(dp[i][j] != -1){
             return dp[i][j];
         }
+
         int ans = 0;
         if(s1.charAt(i) == s2.charAt(j)){
             ans= 1+LCS(s1,s2,i+1,j+1,dp);
@@ -27,6 +28,5 @@ class Solution {
             ans = Math.max(fs,ss);
         }
         return dp[i][j] = ans;
-
     }
 }
