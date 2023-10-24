fe40a3efc456e54a55a8830a398afb40e10ced55
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-10-24 15:54:14 +0530

    Time: 2 ms (82.84%), Space: 44.5 MB (7.58%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -18,6 +18,7 @@ class Solution {
         if(root==null){
              return new ArrayList<>();
         }
+        
         Queue<TreeNode> mq=new ArrayDeque<>();
         mq.add(root);
         List<Integer> result=new ArrayList<>();
