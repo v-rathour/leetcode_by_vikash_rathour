a1507d7176c74fd6c01f9f02409c7ed9920d7ad1
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-10-30 19:29:50 +0530

    Time: 1 ms (99.73%), Space: 44 MB (75.52%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,35 +1,41 @@
 class Solution {
-    
     public int minFallingPathSum(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
         
-        int dp[][] = new int[n][m];
-        
-        for(int i = n-1;i>=0;i--){
-            for(int j = m-1;j>=0;j--){
-                if(i == n-1){
-                    dp[i][j] = matrix[i][j];
-                }
-                else if(j == 0){
-                    dp[i] [j] = Math.min(dp[i+1][j],dp[i+1][j+1])+matrix[i][j];
-                }
-                else if(j == m-1){
-                    dp[i] [j] = Math.min(dp[i+1][j],dp[i+1][j-1])+matrix[i][j];
-                }
-                else{
-                    dp[i] [j] = Math.min(dp[i+1][j],
-                                Math.min(dp[i+1][j+1],dp[i+1][j-1]))
-                                 +matrix[i][j];
-                }
-            }
+        int [][]dp = new int[matrix.length][matrix[0].length];
+        for(int a[]:dp){
+            Arrays.fill(a,Integer.MIN_VALUE);
         }
         
-        int ans = Integer.MAX_VALUE;
-        for(int i = 0;i<m;i++){
-            ans = Math.min(dp[0][i],ans);
+        for (int i = 0; i < matrix[0].length; i++) {
+           Mininumpath(matrix,0,i,dp);
         }
-        return ans;
         
+        int min = Integer.MAX_VALUE;
+        for(int i = 0;i<dp[0].length;i++){
+            min = Math.min(min,dp[0][i]);
+        }
+        return min;
+        
+    }
+    
+    public static int Mininumpath(int [][]matrix,int i,int j,int [][]dp){
+
+        if(i>=matrix.length || j>= matrix[0].length || j<0){
+            return Integer.MAX_VALUE;
+        }
+
+        if(dp[i][j] != Integer.MIN_VALUE){
+            return dp[i][j];
+        }
+
+        int left = Mininumpath(matrix,i+1,j-1,dp);
+        int down = Mininumpath(matrix,i+1,j,dp);
+        int right = Mininumpath(matrix,i+1,j+1,dp);
+        int max = Math.min(left,Math.min(right,down));
+
+        if(max == Integer.MAX_VALUE){
+            return dp[i][j] = matrix[i][j];
+        }
+        return dp[i][j] = matrix[i][j]+max;
     }
 }
