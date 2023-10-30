999e4d2493ec43558d0c9a5e955fc1b3bd4ca201
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-10-30 18:13:01 +0530

    Time: 1 ms (96.73%), Space: 43.4 MB (90.66%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,30 +1,33 @@
 class Solution {
-    
-    public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+    public int minPathSum(int[][] nums) {
         
-        int dp[][] = new int[m][n];
-        
-        for(int i = m-1;i >= 0;i--){
-            for(int j = n-1;j >= 0 ;j--){
-                if(i == m-1 && j == n-1){
-                    dp[i][j] = grid[i][j];
-                }
-                else if(i == m-1){
-                    dp[i][j] = grid[i][j]+dp[i][j+1];
-                }
-                else if(j == n-1){
-                    dp[i][j] = grid[i][j]+dp[i+1][j];
-                }
-                else{
-                    dp[i][j] = grid[i][j]+Math.min(dp[i+1][j],dp[i][j+1]);
-                }
-            }
+        int [][]dp = new int[nums.length][nums[0].length];
+        for(int []a:dp){
+            Arrays.fill(a,-1);
         }
         
-        return dp[0][0];
+        return Minmumpath(nums,0,0,dp);
         
     }
     
+    public static int Minmumpath(int [][]nums,int i,int j,int [][]dp){
+        if(i >= nums.length || j >= nums[0].length){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int row = Minmumpath(nums,i+1,j,dp);
+        int col = Minmumpath(nums,i,j+1,dp);
+
+        int ans = 0;
+        if(i == nums.length-1 || j == nums[0].length-1){
+            ans = nums[i][j]+Math.max(row,col);
+        }
+        else{
+            ans = nums[i][j]+Math.min(row,col);
+        }
+        return dp[i][j] = ans;
+    }
 }
