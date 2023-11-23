b37a08fd545a2ba9a78d79e439e03c0c2e4f45d2
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2023-11-23 10:05:30 +0530

    Time: 32 ms (9.56%), Space: 48.7 MB (14.10%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,32 +1,35 @@
 class Solution {
     public int longestCommonSubsequence(String s1, String s2) {
         
-        int dp[][] = new int[s1.length()][s2.length()];
-        for(int a[] :dp){
+        int [][]dp = new int[s1.length()][s2.length()];
+        for(int a[]:dp){
             Arrays.fill(a,-1);
         }
         
-        return LCS(s1,s2,0,0,dp);
+        return LongestSubsequence(s1,s2,0,0,dp);
+        
     }
     
-    public static int LCS(String s1,String s2,int i, int j,int [][]dp){
-
+    public int LongestSubsequence(String s1,String s2,int i,int j,int [][]dp){
+        
         if(s1.length() == i || s2.length() == j){
             return 0;
         }
-
+        
         if(dp[i][j] != -1){
             return dp[i][j];
         }
-
         int ans = 0;
         if(s1.charAt(i) == s2.charAt(j)){
-            ans= 1+LCS(s1,s2,i+1,j+1,dp);
-        }else{
-            int fs = LCS(s1,s2,i+1,j,dp);
-            int ss = LCS(s1,s2,i,j+1,dp);
+            ans = LongestSubsequence(s1,s2,i+1,j+1,dp)+1;
+        }
+        else{
+            int fs = LongestSubsequence(s1,s2,i+1,j,dp);
+            int ss = LongestSubsequence(s1,s2,i,j+1,dp);
+            
             ans = Math.max(fs,ss);
         }
-        return dp[i][j] = ans;
+        
+        return dp[i][j]=ans;
     }
 }
