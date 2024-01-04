aaf26271a17fcb2761b73b91fdd05570aaacde59
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-01-04 14:49:53 +0530

    Time: 1 ms (99.98%), Space: 56.7 MB (92.06%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,26 @@
 class Solution {
+    
     public int maxSubArray(int[] nums) {
-        int ans=Integer.MIN_VALUE;
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-              sum +=nums[i];
-            if(sum>ans)
-                ans=sum;
+        
+        return kadanesAlgo(nums);
+    }
+    
+    public int kadanesAlgo(int nums[]){
+        int sum = 0;
+        int ans = Integer.MIN_VALUE;
+        for(int i = 0;i<nums.length;i++){
+             sum += nums[i];
+            
+            if(sum>ans){
+                ans = sum;
+            }
+            
             if(sum<0){
-                sum=0;
+                sum = 0;
             }
         }
-           return ans;
+        
+        return ans;
     }
     
 }
