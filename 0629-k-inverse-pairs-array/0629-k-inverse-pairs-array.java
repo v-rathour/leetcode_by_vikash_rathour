6aa0ee655a8a2ce162a53eab0ac21755b68fb397
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-01-27 21:14:49 +0530

    Time: 1136 ms (34.45%), Space: 46.7 MB (34.45%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -2,7 +2,7 @@ class Solution {
     
      public int kInversePairs(int n, int k) {
     
-        int [][]dp = new int[1001][1001];
+        int [][]dp = new int[n+1][k+1];
         
         for(int i =1 ; i<=n ;i++)
         {
@@ -11,8 +11,8 @@ class Solution {
                 if(j == 0)
                      dp[i][j]=1;
                 else
-                for(int m=0 ; m<= Math.min(j,i-1) ; m++)
-                    dp[i][j] = ( dp[i][j] + dp[i-1][j-m] ) % 1000000007;
+                    for(int m=0 ; m<= Math.min(j,i-1) ; m++)
+                        dp[i][j] = ( dp[i][j] + dp[i-1][j-m] ) % 1000000007;
             }
         }
         return dp[n][k];
