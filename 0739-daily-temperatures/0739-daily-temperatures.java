3a1194a750f787b636f1b693693104f1ddd72f84
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-01-31 11:10:53 +0530

    Time: 81 ms (25.71%), Space: 60.4 MB (43.77%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,24 +1,29 @@
 class Solution {
+    
     public int[] dailyTemperatures(int[] temperatures) {
-        return Next_greater(temperatures);
-    }
-    public int[] Next_greater(int[] arr) {
-        int newarr[]=new int[arr.length];
-        Stack<Integer> st=new Stack<>();
-        st.push(arr.length-1);
-        newarr[arr.length-1]=0;
-        for (int i = arr.length-2; i>=0; i--) {
-            while(st.size()>0 && arr[st.peek()]<=arr[i]) {
+        int  n = temperatures.length;
+        Stack<Integer> st = new Stack<>();
+        int []answer = new int[temperatures.length];
+        
+        answer[n-1] = 0;
+        st.push(n-1);
+        for(int i = n-2;i>=0;i--){
+            
+            while(!st.isEmpty() && temperatures[st.peek()]<=temperatures[i]){
                 st.pop();
             }
-            if(st.size()==0){
-                newarr[i]=0;
+            
+            if(st.isEmpty()){
+                answer[i] = 0;
             }
             else{
-                newarr[i]=st.peek()-i;
+                answer[i] = st.peek()-i;
             }
+            
             st.push(i);
         }
-        return newarr;
+        
+        return answer;
+        
     }
 }
