2c4a0d3339ec340a06d1b27e9c606a8971af1114
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-02-28 14:37:28 +0530

    Time: 3 ms (100.00%), Space: 57.3 MB (87.38%) - LeetHub

--- 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
@@ -1,29 +1,29 @@
 class Solution {
-    HashMap<Integer,Long> adj;
+    
     public int edgeScore(int[] edges) {
-        adj = new HashMap<>();
+        long []MaximumSum = new long[edges.length];
         
-        int n = edges.length;
-        for(int i = 0;i<n;i++){
-            adj.put(i,(long)0);
-        }
+        int ans = 0;
+        long max = 0;
         
         for(int i = 0;i<edges.length;i++){
-            adj.put(edges[i],adj.get(edges[i])+(long)i);
-        }
-        
-        long max = 0;
-        int ans = 0;
-        // System.out.println(adj);
-        for(int key : adj.keySet()){
-            long sum = adj.get(key);
-            if(sum>max){
-                ans = key;
-                max = sum;
+            
+            MaximumSum[edges[i]] += i;
+            
+            if(MaximumSum[edges[i]]>max){
+                ans = edges[i];
+                max = MaximumSum[edges[i]];
+            }
+            
+            if(MaximumSum[edges[i]] == max){
+                ans = Math.min(edges[i],ans);
             }
         }
         
+        
         return ans;
+        
+        
         
     }
 }
