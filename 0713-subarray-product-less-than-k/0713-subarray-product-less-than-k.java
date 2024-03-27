167294904f933961654b74ae43f63e6cfe19733d
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-03-27 06:05:30 +0530

    Time: 4 ms (100.00%), Space: 47.6 MB (49.37%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,26 +1,29 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        return SubArray(nums,k);
         
-    }
-    public static int SubArray(int[] nums,int k){
-        int ans=0;
-        int si=0;
-        int ei=0;
-        int p=1;
-        while(ei<nums.length){
-            //grow
-            p*=nums[ei];
-            //shrik
-            while(p>=k && si<=ei){
-                p/=nums[si];
+        int si = 0;
+        int ei = 0;
+        int n = nums.length;
+        int sum = 1;
+        
+        int ans = 0;
+        
+        while(ei<n){
+            
+            sum *= nums[ei];
+            
+            while(sum>=k && si<=ei){
+                sum /= nums[si];
                 si++;
             }
-            //anscalculate
-            ans=ans+(ei-si+1);
+            
+            ans += (ei-si+1);
+            
             ei++;
+            
         }
+        
         return ans;
-    
-}
+        
+    }
 }
