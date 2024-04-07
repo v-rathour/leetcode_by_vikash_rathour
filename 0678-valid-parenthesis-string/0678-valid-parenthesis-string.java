d9bcc4a289df671b9f59b8a5276d3235c3f13180
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-04-07 07:29:07 +0530

    Time: 1 ms (38.41%), Space: 41.3 MB (57.11%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -5,19 +5,16 @@ class Solution {
         
         for(int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            //If it's opening bracket, push them in a stack
+            
             if(ch == '(') {
                 open.push(i);
             }
-            // If it's a star then push them in a second stack
+            
             else if(ch == '*') {
                 star.push(i);
             }
             else {
-               /* If the character is closing bracket, then there are three cases.
-               1) If the first stack is not empty, first pop them from that stack. 
-               2) If it's empty check in a second stack, Pop them from that.
-               3) If both of them empty then return false. */
+               
                 if(!open.isEmpty()) {
                     open.pop();
                 }
@@ -30,18 +27,18 @@ class Solution {
             }
         }
         
-        // Now process leftover opening bracket
+       
         while(!open.isEmpty()) 
         {
             if(star.isEmpty()) {
                 return false;
             }
-            // star is lying to the right hand side of opening bracket
+            
             else if(open.peek() < star.peek()) {
                 open.pop();
                 star.pop();
             }
-            else {          // CASE: open.top() > start.pop() 
+            else {          
                 return false;
             }
         }
