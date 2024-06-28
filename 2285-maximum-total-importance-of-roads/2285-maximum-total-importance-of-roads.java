6c4502c6ac488150da0e4d33f4363120fb9249da
vikash rathour <112382180+v-rathour@users.noreply.github.com>
2024-06-28 23:22:44 +0530

    Time: 93 ms (7.03%), Space: 71.6 MB (14.06%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.java
@@ -1,25 +1,61 @@
 class Solution {
-    public long maximumImportance(int n, int[][] roads) {
+    
+    class Pair implements Comparable<Pair>{
+        int val;
+        int size;
         
-        int edges[] = new int[n];
-        
-        for(int i=0;i<roads.length;i++){
-            
-            int x = roads[i][0];
-            int y = roads[i][1];
-            
-            edges[x]++;
-            edges[y]++;
+        Pair(int val,int size){
+            this.val = val;
+            this.size = size;
         }
         
-        Arrays.sort(edges);
+        public int compareTo(Pair obj){
+            return (this.size == obj.size)?(this.val-obj.val):(obj.size-this.size);
+        }
+    }
+    
+    public long maximumImportance(int n, int[][] roads) {
         
-        long ans = 0;
-        for(int i=n-1;i >= 0;i--){
+        HashMap<Integer,List<Integer>> adj = new HashMap<>();
+        
+        for(int i = 0;i<n;i++){
+            adj.put(i,new ArrayList<>());
+        }
+        
+        for(int a[] : roads){
             
-            ans = ans + (long)edges[i]*(long)(i+1);
+            int u = a[0];
+            int v = a[1];
+            
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+            
+        }
+        
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        
+        for(int key :adj.keySet()){
+            pq.add(new Pair(key,adj.get(key).size()));
+        }
+        
+        int []assign = new int[n];
+        int count=n;
+        while(!pq.isEmpty()){
+            Pair rem = pq.remove();
+            
+            assign[rem.val] = count;
+            count--;
+            
+        }
+        
+       long ans = 0;
+        
+        for(int a[] : roads){
+            ans += assign[a[0]];
+            ans += assign[a[1]];
         }
         
         return ans;
+        
     }
 }
